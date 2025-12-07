c24e4463c6046547659142c4d8c0d6d573457e59
Nakul-Kumar-Singh-Baboolall <nakul.baboolall@umail.uom.ac.mu>
2025-12-07 19:10:56 +0400

    Added php code to connect the database to the different web page - this is the database connection file

--- src/db.php
+++ src/db.php
@@ -1,1 +1,15 @@
+<?php
+$host = "localhost";
+$dbname = "ryans_coffee_and_pastries_db";
+$username = "root";
+$password = "";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
 
